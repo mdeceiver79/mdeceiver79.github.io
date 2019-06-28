836b1a1cbdb130f99a23120095015f12eec6bb1c
Oliver Murray <omurray@gs-systems.co.uk>
2019-06-28 14:41:09 +0100

    spacehulk1

---
+++ Spacehulk/app.js
@@ -0,0 +1,17 @@
+// render() {
+  // const elements = ['one', 'two', 'three'];
+
+  // const items = []
+
+  // for (const [index, value] of elements.entries()) {
+    // items.push(<li key={index}>{value}</li>)
+  // }
+
+  // return (
+    // <div>
+      // {items}
+    // </div>
+  // )
+// }
+const element = <h1>Hello React!</h1>
+ReactDOM.render(element, document.getElementById('root'));
