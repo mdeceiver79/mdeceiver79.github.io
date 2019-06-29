8ad9e9a14907410e29f353c21bf5bac70dc29e75
Fulcrum <mdeceiver79@hotmail.co.uk>
2019-06-29 17:34:51 +0100

    Added a bunch more stuff

--- Spacehulk/app.js
+++ Spacehulk/app.js
@@ -34,6 +34,10 @@ app.config(function($stateProvider) {
 		name: 'squadCreator',
 		url: '/squadCreator'
 	}
+	var npcs = {
+		name: 'npcs',
+		url: '/npcs'
+	}
   $stateProvider.state(portal);
   $stateProvider.state(weapons);
   $stateProvider.state(weapon);
@@ -42,6 +46,7 @@ app.config(function($stateProvider) {
   $stateProvider.state(squads);
   $stateProvider.state(squad);
   $stateProvider.state(squadCreator);
+  $stateProvider.state(npcs);
 });
 
 app.controller("PageController", ["$scope", "$state", function($scope, $state) {
@@ -63,6 +68,93 @@ app.controller("PageController", ["$scope", "$state", function($scope, $state) {
 		itemPuritySeal,
 		itemStims,
 	];
+	$scope.npcs = [
+		{
+			name : "Purestrain",
+			faction : "Genestealer",
+			ap : 6,
+			melee : "3D6",
+			range : null,
+			rules : []
+		},
+		{
+			name : "Acolyte Hybrid",
+			faction : "Genestealer",
+			ap : 4,
+			melee : "2D6",
+			range : null,
+			rules : []
+		},
+		{
+			name : "Neophyte Hybrid",
+			faction : "Genestealer",
+			ap : 4,
+			melee : "1D6-2",
+			range : null,
+			rules : []
+		},
+		
+	];
+}]);
+
+app.controller("WeaponsController", ["$scope", function($scope) {
+	$scope.layout = "table";
+	$scope.filter = null;
+	$scope.sortBy = null;
+	$scope.sortByReverse = false;
+	$scope.changeSort = (sortby) => {
+		if ($scope.sortBy === sortby) {
+			if (!$scope.sortByReverse) {
+				$scope.sortByReverse = true;
+			} else {
+				$scope.sortBy = null;
+				$scope.sortByReverse = false;
+			}
+		} else {
+			$scope.sortBy = sortby;
+			$scope.sortByReverse = false;
+		}
+	};
+}]);
+
+app.controller("ItemsController", ["$scope", function($scope) {
+	$scope.layout = "table";
+	$scope.filter = null;
+	$scope.sortBy = null;
+	$scope.sortByReverse = false;
+	$scope.changeSort = (sortby) => {
+		if ($scope.sortBy === sortby) {
+			if (!$scope.sortByReverse) {
+				$scope.sortByReverse = true;
+			} else {
+				$scope.sortBy = null;
+				$scope.sortByReverse = false;
+			}
+		} else {
+			$scope.sortBy = sortby;
+			$scope.sortByReverse = false;
+		}
+	};
+}]);
+
+app.controller("NpcsController", ["$scope", function($scope) {
+	$scope.layout = "table";
+	$scope.filter = null;
+	$scope.sortBy = null;
+	$scope.sortByReverse = false;
+	$scope.changeSort = (sortby) => {
+		if ($scope.sortBy === sortby) {
+			if (!$scope.sortByReverse) {
+				$scope.sortByReverse = true;
+			} else {
+				$scope.sortBy = null;
+				$scope.sortByReverse = false;
+			}
+		} else {
+			$scope.sortBy = sortby;
+			$scope.sortByReverse = false;
+		}
+	};
 }]);
 
 app.directive('ngContextualLink', function ($compile) {
